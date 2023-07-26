9897adb1fcd46c6912e552f7d50ed19ac5430052
Austin Chauncey <austinchauncey@chief.com>
2023-07-26 17:18:08 -0500

    chore: some more upgrades

--- src/heroku-db/adapter.ts
+++ src/heroku-db/adapter.ts
@@ -1,7 +1,7 @@
 import postgres from "postgres";
 import { env } from "../env";
 
-export class HerokuDatabase {
+export class HerokuDatabaseAdapter {
   sql = postgres(env.HEROKU_DATABASE_URL, {
     ssl: { rejectUnauthorized: false },
   });
